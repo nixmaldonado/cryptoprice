42659ce0950d5ac002343fb4b37db300db01bca6
nicolas.maldonado <nicolas.maldonado@globant.com>
2018-01-11 15:32:42 -0300

    Removes ViewModel and Repository implementation

--- app/src/main/java/com/globant/cryptoprice/MainActivity.kt
+++ app/src/main/java/com/globant/cryptoprice/MainActivity.kt
@@ -1,21 +1,15 @@
 package com.globant.cryptoprice
 
-import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.globant.cryptoprice.quotation.Controller
-import com.globant.cryptoprice.repository.PricesRepository
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val LIST_ID = "listId"
     private lateinit var linearLayoutManager: LinearLayoutManager
-    private val repository = PricesRepository()
-    private var viewModel = CurrenciesViewModel(repository)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +17,6 @@ class MainActivity : AppCompatActivity() {
 
         val controller = Controller(this)
         controller.start()
-
-        viewModel = ViewModelProviders.of(this).get(CurrenciesViewModel::class.java)
-        viewModel.init()
-
-        viewModel.getCurrentCurrencyList()?.observe(this, Observer {
-            controller.recyclerAdapter.notifyDataSetChanged()
-        })
 
         linearLayoutManager = LinearLayoutManager(this)
         recycler_view.layoutManager = linearLayoutManager
